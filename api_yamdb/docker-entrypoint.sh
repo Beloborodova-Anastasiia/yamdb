4e326ef4f95a1e238a111e5b757f61760feba7f5
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2022-09-07 16:46:14 +0500

    Final

--- api_yamdb/docker-entrypoint.sh
+++ api_yamdb/docker-entrypoint.sh
@@ -7,8 +7,8 @@ echo "Apply database migrations"
 python manage.py makemigrations reviews
 python manage.py migrate
 
-echo "Load fixtures"
-python manage.py loaddata fixtures.json
+# echo "Load fixtures"
+# python manage.py loaddata fixtures.json
 
 echo "Starting server"
 gunicorn api_yamdb.wsgi:application --bind 0:8000
