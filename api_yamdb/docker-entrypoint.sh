b8ef635bb884aad72d35060522e20b427b6115f9
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2022-09-07 16:20:34 +0500

    Test

--- api_yamdb/docker-entrypoint.sh
+++ api_yamdb/docker-entrypoint.sh
@@ -3,12 +3,12 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-echo "Apply database migrations"
-python manage.py makemigrations reviews
-python manage.py migrate
+# echo "Apply database migrations"
+# python manage.py makemigrations reviews
+# python manage.py migrate
 
-echo "Load fixtures"
-python manage.py loaddata fixtures.json
+# echo "Load fixtures"
+# python manage.py loaddata fixtures.json
 
 echo "Starting server"
 gunicorn api_yamdb.wsgi:application --bind 0:8000
